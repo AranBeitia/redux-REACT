32e24e8f801fe66dd23b5d47a3e28c8a31005bfa
AranBeitia <arantza.beitia@gmail.com>
2023-07-04 14:51:01 +0200

    Feat: login redux

--- redux-project/src/App.js
+++ redux-project/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as HelloKitty, Routes, Route } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 function App() {
 	return (
-		<>
+		<HelloKitty>
 			<h1>app</h1>
-		</>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</HelloKitty>
 	)
 }
 
---
+++ redux-project/src/components/Register/Register.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { register } from '../../features/auth/authSlice'
+import { notification } from 'antd'
+
+const Register = () => {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+		age: 0,
+	})
+	const { name, email, password, password2, age } = formData
+	const dispatch = useDispatch()
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		if (password !== password2) {
+			return notification.error({
+				message: 'Error',
+				description: 'Passwords do not match',
+			})
+		} else {
+			notification.success({
+				message: 'Success',
+				description: 'Correct',
+			})
+			return dispatch(register(formData))
+		}
+	}
+	return (
+		<form onSubmit={onSubmit}>
+			<input
+				type="text"
+				name="name"
+				value={name}
+				onChange={onChange}
+				placeholder="escribe tu nombre"
+			/>
+			<input
+				type="email"
+				name="email"
+				value={email}
+				onChange={onChange}
+				placeholder="escrube tu email"
+			/>
+			<input
+				type="number"
+				name="age"
+				value={age}
+				onChange={onChange}
+				placeholder="escrube tu edad"
+			/>
+			<input
+				type="password"
+				name="password"
+				value={password}
+				onChange={onChange}
+				placeholder="escribe tu pass"
+			/>
+			<input
+				type="password"
+				name="password2"
+				value={password2}
+				onChange={onChange}
+				placeholder="Repite tu pass"
+			/>
+			<button type="submit">Register</button>
+		</form>
+	)
+}
+export default Register
